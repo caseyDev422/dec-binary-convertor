96a73f827f4ce5b20c6307746814612996b61525
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-20 14:52:14 -0500

    v1 - both calc working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState("");
+  const [decCount, setDecCount] = useState(0);
   const [binary, setBinary] = useState(0);
 
   function useInput() {
@@ -16,57 +17,24 @@ function App(props) {
     return [count, input];
   }
 
-  // const [decimal, setDecimal] = useState(0);
-
-  // function Binary() {
-  //   const [decimal, setDecimal] = useState(0);
-  //   const decimalBinary = (
-  //     <>
-  //       <h1>{decimal}</h1>
-  //       <input
-  //         value={decimal}
-  //         onChange={(e) => setDecimal(e.target.value)}
-  //         type="number"
-  //       />
-  //     </>
-  //   );
-  //   return [decimalBinary, decimal];
-  // }
-
-  // function binaryConvert() {
-  //   setDecimal(decimal.toString(2));
-  // }
-
-  // function ResetDecimal() {
-  //   const [value, setDecimalZero] = useInput(0);
-  //   const input = (
-  //     <input
-  //       value={value}
-  //       type="number"
-  //       onChange={(e) => setDecimalZero(e.target.value)}
-  //     />
-  //   );
-  //   return [input, value];
-  // }
-
   function increase() {
-    setCount(count + 1);
+    setDecCount(decCount + 1);
   }
 
-  function increaseTen() {
-    setCount(count + 10);
+  function plusTen() {
+    setDecCount(decCount + 10);
   }
 
-  function increaseNine() {
-    setCount(count + 10);
+  function plusTwnety() {
+    setDecCount(decCount + 20);
   }
 
-  function increaseHundred() {
-    setCount(count + 100);
+  function plusHundred() {
+    setDecCount(decCount + 100);
   }
 
-  function increaseHundredOne() {
-    setCount(count + 101);
+  function plusThousand() {
+    setDecCount(decCount + 1000);
   }
 
   function decrease() {
@@ -81,15 +49,11 @@ function App(props) {
   }
 
   function binaryToDec() {
-    setCount(count.toString(2));
+    setDecCount(decCount.toString(2));
   }
 
-  function plusZero() {
-    setCount(count * 10);
-  }
-
-  function plusOne() {
-    setCount(count + 0 + 1);
+  function plusFive() {
+    setDecCount(decCount + 5);
   }
 
   function resetBinary() {
@@ -97,7 +61,7 @@ function App(props) {
   }
 
   function reset() {
-    setCount(0);
+    setDecCount(0);
   }
 
   function inputDecimal(e) {
@@ -106,36 +70,57 @@ function App(props) {
 
   return (
     <>
-      <div className="container">
-        <h2>Binary to Decimal</h2>
-        <h1>{binary}</h1>
-        <input
-          onChange={inputDecimal}
-          type="number"
-          placeholder="Enter base10 Number"
-          value={count}
-        />
-        <button onClick={decToBinary} value={count}>
-          conv
-        </button>
-        <button onClick={resetBinary}>reset</button>
+      <div className="header">
+        <div className="container-fluid">
+          <div className="container">
+            <div className="calc-container">
+              <h2>Binary to Decimal</h2>
+              <h1>{binary}</h1>
+              <input
+                onChange={inputDecimal}
+                type="number"
+                placeholder="Enter Binary Number"
+                value={count}
+              />
+              <button onClick={decToBinary} value={count}>
+                conv
+              </button>
+              <button onClick={resetBinary}>reset</button>
+            </div>
+          </div>
+          <div className="container">
+            <div className="calc-container">
+              <h2>Decimal to Binary</h2>
+              <h1>{decCount}</h1>
+              <button onClick={increase} value={decCount}>
+                +1
+              </button>
+              <button onClick={plusFive} value={decCount}>
+                +5
+              </button>
+              <button onClick={plusTen} value={decCount}>
+                +10
+              </button>
+              <button onClick={plusTwnety} value={decCount}>
+                +20
+              </button>
+              <button onClick={plusHundred} value={decCount}>
+                +100
+              </button>
+              <button onClick={plusThousand} value={decCount}>
+                +1000
+              </button>
+              <button onClick={binaryToDec} value={decCount}>
+                conv
+              </button>
+              <button onClick={reset} value={decCount}>
+                reset
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="footer"></div>
       </div>
-      <div className="container">
-        <h2>Decimal to Binary</h2>
-        <h1>{count}</h1>
-        <button onClick={plusOne}>+1</button>
-        <button onClick={plusZero}>+0</button>
-        <button onClick={increaseNine}>+10</button>
-        <button onClick={increaseHundredOne}>+101</button>
-        <button onClick={binaryToDec}>conv</button>
-        <button onClick={reset}>reset</button>
-      </div>
-      <useInput />
-      {/* <div className="container">
-        <h3>Decimal - Binary</h3>
-        <Binary />
-        <button onClick={binaryConvert}>=</button>
-      </div> */}
     </>
   );
 }
