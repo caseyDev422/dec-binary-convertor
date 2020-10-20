f26693c163cd516606fa1a5c7a5e1886a4677f67
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-19 20:31:05 -0500

    Dec-Binary works Binary-Dec does not work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,20 +17,20 @@ function App(props) {
 
   const [decimal, setDecimal] = useState(0);
 
-  function Binary() {
-    const [decimal, setDecimal] = useState(0);
-    const decimalBinary = (
-      <>
-        <h1>{decimal}</h1>
-        <input
-          value={decimal}
-          onChange={(e) => setDecimal(e.target.value)}
-          type="number"
-        />
-      </>
-    );
-    return [decimalBinary, decimal];
-  }
+  // function Binary() {
+  //   const [decimal, setDecimal] = useState(0);
+  //   const decimalBinary = (
+  //     <>
+  //       <h1>{decimal}</h1>
+  //       <input
+  //         value={decimal}
+  //         onChange={(e) => setDecimal(e.target.value)}
+  //         type="number"
+  //       />
+  //     </>
+  //   );
+  //   return [decimalBinary, decimal];
+  // }
 
   function binaryConvert() {
     setDecimal(decimal.toString(2));
@@ -52,12 +52,26 @@ function App(props) {
     setCount(count + 1);
   }
 
+  function increaseTen() {
+    setCount(count + 10);
+  }
+
+  function increaseHundred() {
+    setCount(count + 100);
+  }
+
   function decrease() {
     setCount(count - 1);
   }
 
-  function multiply() {
+  function decToBinary() {
     setCount(count.toString(2));
+  }
+
+  function binaryToDec() {
+    var countInt = count;
+    var digit = parseInt(countInt, 2);
+    setCount(digit);
   }
 
   function reset() {
@@ -67,17 +81,30 @@ function App(props) {
   return (
     <>
       <div className="container">
+        <h2>Decimal to Binary</h2>
         <h1>{count}</h1>
         <button onClick={decrease}>-</button>
         <button onClick={increase}>+</button>
-        <button onClick={multiply}>*10</button>
-        <button onClick={reset}>0</button>
+        <button onClick={increaseTen}>+10</button>
+        <button onClick={increaseHundred}>+100</button>
+        <button onClick={decToBinary}>conv</button>
+        <button onClick={reset}>reset</button>
       </div>
       <div className="container">
+        <h2>Binary to Decimal</h2>
+        <h1>{count}</h1>
+        <button onClick={decrease}>-</button>
+        <button onClick={increase}>+</button>
+        <button onClick={increaseTen}>+10</button>
+        <button onClick={increaseHundred}>+100</button>
+        <button onClick={binaryToDec}>conv</button>
+        <button onClick={reset}>reset</button>
+      </div>
+      {/* <div className="container">
         <h3>Decimal - Binary</h3>
         <Binary />
         <button onClick={binaryConvert}>=</button>
-      </div>
+      </div> */}
     </>
   );
 }
