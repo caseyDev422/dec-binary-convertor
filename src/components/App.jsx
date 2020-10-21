62e9d81ec29182c260c1a60c1a2570889f4a6a40
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-21 16:11:57 -0500

    css Updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -54,56 +54,57 @@ function App(props) {
 
   return (
     <>
-      <div className="header">
-        <div className="container-fluid">
-          <div className="container">
-            <div className="calc-container">
-              <h2>Binary to Decimal</h2>
-              <h1>{binary}</h1>
-              <input
-                onChange={inputDecimal}
-                type="number"
-                placeholder="Enter Binary Number"
-                value={count}
-              />
-              <button onClick={decToBinary} value={count}>
-                conv
-              </button>
-              <button onClick={resetBinary}>reset</button>
-            </div>
-          </div>
-          <div className="container">
-            <div className="calc-container">
-              <h2>Decimal to Binary</h2>
-              <h1>{decCount}</h1>
-              <button onClick={increase} value={decCount}>
-                +1
-              </button>
-              <button onClick={plusFive} value={decCount}>
-                +5
-              </button>
-              <button onClick={plusTen} value={decCount}>
-                +10
-              </button>
-              <button onClick={plusTwnety} value={decCount}>
-                +20
-              </button>
-              <button onClick={plusHundred} value={decCount}>
-                +100
-              </button>
-              <button onClick={plusThousand} value={decCount}>
-                +1000
-              </button>
-              <button onClick={binaryToDec} value={decCount}>
-                conv
-              </button>
-              <button onClick={reset} value={decCount}>
-                reset
-              </button>
-            </div>
+      <div className="header"></div>
+      <div className="container-fluid">
+        <div className="container">
+          <h2>Binary to Decimal</h2>
+          <h1 className="gold-color">{binary}</h1>
+          <div className="calc-container">
+            <input
+              onChange={inputDecimal}
+              type="number"
+              placeholder="Enter Binary Number"
+              value={count}
+            />
+            <button onClick={decToBinary} value={count}>
+              conv
+            </button>
+            <button onClick={resetBinary}>reset</button>
           </div>
         </div>
-        <div className="footer"></div>
+        <div className="container">
+          <h2>Decimal to Binary</h2>
+          <h1 className="gold-color">{decCount}</h1>
+          <div className="calc-container">
+            <button onClick={increase} value={decCount}>
+              +1
+            </button>
+            <button onClick={plusFive} value={decCount}>
+              +5
+            </button>
+            <button onClick={plusTen} value={decCount}>
+              +10
+            </button>
+            <button onClick={plusTwnety} value={decCount}>
+              +20
+            </button>
+            <button onClick={plusHundred} value={decCount}>
+              +100
+            </button>
+            <button onClick={plusThousand} value={decCount}>
+              +1000
+            </button>
+            <button onClick={binaryToDec} value={decCount}>
+              conv
+            </button>
+            <button onClick={reset} value={decCount}>
+              reset
+            </button>
+          </div>
+        </div>
+        <div className="footer footer-text">
+          Why write when button do trick?
+        </div>
       </div>
     </>
   );
